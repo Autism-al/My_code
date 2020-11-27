6687e17f34199995a378168b03268fdcfeaf8402
AYuaner <49134217+AYuaner@users.noreply.github.com>
2020-11-27 20:35:32 +0800

    Add files via upload

---
+++ ARIA/pages/VipDay/VipDay.js
@@ -0,0 +1,95 @@
+// pages/VIP/VIP.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    array:[
+      {
+        name:'海南航空',
+        url:'https://s3.ax1x.com/2020/11/21/DlIBNj.png',
+        day:'-8-',
+      },
+      {
+        name:'山东航空',
+        url:'https://s3.ax1x.com/2020/11/25/DasS9s.jpg',
+        day:'-8-',
+      },{
+        name:'厦门航空',
+        url:'https://s3.ax1x.com/2020/11/25/Dasn3R.jpg',
+        day:'-9-',
+      },{
+        name:'深圳航空',
+        url:'https://s3.ax1x.com/2020/11/27/DrgUl8.jpg',
+        day:'-12-',
+      },{
+        name:'昆明航空',
+        url:'https://s3.ax1x.com/2020/11/27/DrgeW6.png',
+        day:'-16-',
+      }
+  ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  /*
+  tapname: function() 
+  {
+    wx.navigateTo({url:'../../myself/VResult/VResult'})
+  }
+*/
+})
